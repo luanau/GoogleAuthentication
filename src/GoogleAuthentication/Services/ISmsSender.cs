3debc1d1ef4ef83bee727d5b5cc417321e7907cf
Luan Au <luanau@gmail.com>
2017-03-03 21:40:12 +0000

    ASP.NET Core web app with google authentication

---
+++ src/GoogleAuthentication/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleAuthentication.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
